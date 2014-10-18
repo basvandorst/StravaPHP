f82c6ea71cef6183c5398c88a56021bf5c3a55c9
Bas van Dorst <basvandorst@gmail.com>
2014-10-18 20:34:49 +0200

    user details in OAuth class

--- src/Strava/API/OAuth.php
+++ src/Strava/API/OAuth.php
@@ -34,26 +34,41 @@ class OAuth extends \League\OAuth2\Client\Provider\AbstractProvider
         return 'https://www.strava.com/oauth/token';
     }
 
-    public function urlUserDetails(AccessToken $token)
+    public function urlUserDetails(\League\OAuth2\Client\Token\AccessToken $token)
     {
         return 'https://www.strava.com/api/v3/athlete/?access_token='.$token;
     }
 
-    public function userDetails($response, AccessToken $token) {}
+    public function userDetails($response, \League\OAuth2\Client\Token\AccessToken $token)
+    {
+        $user = new User;
 
-    public function userUid($response, AccessToken $token) 
+        $user->exchangeArray(array(
+            'uid' => $response->id,
+            'name' => implode(" ", array($response->firstname, $response->lastname)),
+            'firstName' => $response->firstname,
+            'lastName' => $response->lastname,
+            'email' => $response->email,
+            'location' => $response->country,
+            'imageUrl' => $response->profile,
+            'gender' => $response->sex,
+        ));
+
+        return $user;
+    }
+
+    public function userUid($response, \League\OAuth2\Client\Token\AccessToken $token)
     {
         return $response->id;
     }
-    
-    public function userEmail($response, AccessToken $token)
+
+    public function userEmail($response, \League\OAuth2\Client\Token\AccessToken $token)
     {
         return isset($response->email) && $response->email ? $response->email : null;
     }
-    
-    public function userScreenName($response, AccessToken $token)
-    {
-        return array($response->firstname, $response->lastname);
-    }
 
+    public function userScreenName($response, \League\OAuth2\Client\Token\AccessToken $token)
+    {
+        return implode(" ", array($response->firstname, $response->lastname));
+    }
 }
