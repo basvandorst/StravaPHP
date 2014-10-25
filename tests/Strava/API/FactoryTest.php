89995383dfa1a89b47afda0a3b82c1874efe7074
Bas van Dorst <basvandorst@gmail.com>
2014-10-25 19:26:47 +0200

    Factory test

---
+++ tests/Strava/API/FactoryTest.php
@@ -0,0 +1,16 @@
+<?php
+
+class FactoryTest extends PHPUnit_Framework_TestCase
+{    
+    public function testGetOAuthInstance() {
+        $factory = new Strava\API\Factory();
+        $client = $factory->getOAuthClient(123,'TOKEN','URL');
+        $this->assertInstanceOf('Strava\API\OAuth', $client);
+    }
+    
+    public function testGetAPIClientInstance() {
+        $factory = new Strava\API\Factory();
+        $client = $factory->getAPIClient('TOKEN');
+        $this->assertInstanceOf('Strava\API\Client', $client);
+    }
+}
